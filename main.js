77c5b4eea519b68d70f7061abbb957957fa43f6c
stephenlai <stephenlai2015@gmail.com>
2020-09-10 12:23:38 +0800

    commit

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 const root = document.querySelector('html')
 const deviceWidth = root.clientWidth
+root.style.fontSize = ( deviceWidth / 375 ) * 16 + 'px'
 
 // 設計稿 375px
 // 默認字大小 16px
 
-root.style.fontSize = ( deviceWidth / 375 ) * 16 + 'px'
+window.addEventListener('resize', () => {
+  root.style.fontSize = ( deviceWidth / 375 ) * 16 + 'px'
+})
+
 // root.style.fontSize = `$( deviceWidth / 375 ) * 16}px`
